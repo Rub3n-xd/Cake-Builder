16c93ac9a99c5fc8d41756640ca3ff85a841a78e
Rub3n-xd <73437475+Rub3n-xd@users.noreply.github.com>
2021-09-10 11:59:43 -0400

    Añadido selector de color
    Se añadió un selector de color, un input:text no funcional por el momento para escoger el id y otro para escoger la(s) clase(s), añadidos botones para ocultar el código y otro para ejecutarlo (este no está funcional por el momento).

--- cake_builder.js
+++ cake_builder.js
@@ -10,23 +10,82 @@ function Messages(ev)
 
 var iframe = document.getElementById('iframe').contentWindow;
 
-document.getElementById('p_button').onclick = function() {
-    var message =  "p" + document.getElementById('p_text').value;
+var text = document.getElementById('text')
 
+document.getElementById('p_button').onclick = function() {
+    let text_content = text.value
+    for(let x = 0; text_content.includes(","); x++) {
+        text_content = text_content.replace(",","\n")
+    } 
+    let message =  "p," + document.getElementById('color_text').value + "," + text_content;
+    console.log(message)
     iframe.postMessage(message, '*' );
 }
 document.getElementById('h1_button').onclick = function() {
-    var message =  "1" + document.getElementById('h1_text').value;
+    let text_content = text.value
+    for(let x = 0; text_content.includes(","); x++) {
+        text_content = text_content.replace(",","\n")
+    } 
+    let message =  "h1," + document.getElementById('color_text').value + "," + text_content;
 
     iframe.postMessage(message, '*' );
 }
 document.getElementById('h2_button').onclick = function() {
-    var message =  "2" + document.getElementById('h2_text').value;
+    let text_content = text.value
+    for(let x = 0; text_content.includes(","); x++) {
+        text_content = text_content.replace(",","\n")
+    } 
+    let message =  "h2," + document.getElementById('color_text').value + "," + text_content;
+
+    iframe.postMessage(message, '*' );
+}
+document.getElementById('h3_button').onclick = function() {
+    let text_content = text.value
+    for(let x = 0; text_content.includes(","); x++) {
+        text_content = text_content.replace(",","\n")
+    } 
+    let message =  "h3," + document.getElementById('color_text').value + "," + text_content;
+
+    iframe.postMessage(message, '*' );
+}
+document.getElementById('h4_button').onclick = function() {
+    let text_content = text.value
+    for(let x = 0; text_content.includes(","); x++) {
+        text_content = text_content.replace(",","\n")
+    } 
+    let message =  "h4," + document.getElementById('color_text').value + "," + text_content;
+
+    iframe.postMessage(message, '*' );
+}
+document.getElementById('h5_button').onclick = function() {
+    let text_content = text.value
+    for(let x = 0; text_content.includes(","); x++) {
+        text_content = text_content.replace(",","\n")
+    } 
+    let message =  "h5," + document.getElementById('color_text').value + "," + text_content;
+
+    iframe.postMessage(message, '*' );
+}
+document.getElementById('h6_button').onclick = function() {
+    let text_content = text.value
+    for(let x = 0; text_content.includes(","); x++) {
+        text_content = text_content.replace(",","\n")
+    } 
+    let message =  "h6," + document.getElementById('color_text').value + "," + text_content;
 
     iframe.postMessage(message, '*' );
 }
 document.getElementById('source_button').onclick = function() {
-    var message =  "\n"
+    let message =  "\n"
 
     iframe.postMessage(message, '*' );
+
+    document.getElementById("source_code").classList.add("display")
+}
+document.getElementById('hide_source').onclick = function() {
+    let message =  "\n"
+
+    iframe.postMessage(message, '*' );
+
+    document.getElementById("source_code").classList.remove("display")
 }
--- cake_builder_target.js
+++ cake_builder_target.js
@@ -1,36 +1,38 @@
-var id_count = 0
 window.addEventListener("message", Messages, false);
+window.parent.postMessage(source_code(), '*')
 
 function Messages(ev)
 { 
     console.log(ev.origin, typeof ev.data, ev.data);
 
     if(typeof ev.data == "string") {
-        id_count++
+        let data = ev.data.split(",")
 
-        if(ev.data.startsWith("p"))
-            document.body.innerHTML += "        <p id=\""+id_count+"\">"+ ev.data.substring(1) +"</p>\n"
+        for(let x = 0; data[2].includes("\n"); x++) {
+            data[2] = data[2].replace("\n",",")
+        }
 
-        else if(ev.data.startsWith("1"))
-            document.body.innerHTML += "        <h1 id=\""+id_count+"\">"+ ev.data.substring(1) +"</h1>\n"
+        if(data[0].startsWith("p"))
+            document.body.innerHTML += "        <p style=\"color: "+ data[1] +"\">"+ data[2] +"</p>\n"
 
-        else if(ev.data.startsWith("2"))
-            document.body.innerHTML += "        <h2 id=\""+id_count+"\">"+ ev.data.substring(1) +"</h2>\n"
+        else if(data[0].startsWith("h1"))
+            document.body.innerHTML += "        <h1 style=\"color: "+ data[1] +"\">"+ data[2] +"</h1>\n"
 
-        else if(ev.data.startsWith("3"))
-            document.body.innerHTML += "        <h3 id=\""+id_count+"\">"+ ev.data.substring(1) +"</h3>\n"
+        else if(data[0].startsWith("h2"))
+            document.body.innerHTML += "        <h2 style=\"color: "+ data[1] +"\">"+ data[2] +"</h2>\n"
 
-        else if(ev.data.startsWith("4"))
-            document.body.innerHTML += "        <h4 id=\""+id_count+"\">"+ ev.data.substring(1) +"</h4>\n"
+        else if(data[0].startsWith("h3"))
+            document.body.innerHTML += "        <h3 style=\"color: "+ data[1] +"\">"+ data[2] +"</h3>\n"
 
-        else if(ev.data.startsWith("5"))
-            document.body.innerHTML += "        <h5 id=\""+id_count+"\">"+ ev.data.substring(1) +"</h5>\n"
+        else if(data[0].startsWith("h4"))
+            document.body.innerHTML += "        <h4 style=\"color: "+ data[1] +"\">"+ data[2] +"</h4>\n"
 
-        else if(ev.data.startsWith("6"))
-            document.body.innerHTML += "        <h6 id=\""+id_count+"\">"+ ev.data.substring(1) +"</h6>\n"
-        
-        else if(ev.data.startsWith("\n"))
-            window.parent.postMessage(source_code(), '*')
+        else if(data[0].startsWith("h5"))
+            document.body.innerHTML += "        <h5 style=\"color: "+ data[1] +"\">"+ data[2] +"</h5>\n"
+
+        else if(data[0].startsWith("h6"))
+            document.body.innerHTML += "        <h6 style=\"color: "+ data[1] +"\">"+ data[2] +"</h6>\n"
+
         window.parent.postMessage(source_code(), '*')
     }
 }
@@ -40,4 +42,12 @@ function Messages(ev)
 function source_code() {
     let code = "<!DOCTYPE html>\n<html>\n    <head>\n        " + document.head.innerHTML + "\n    </head>\n    <body>\n        " + document.body.innerHTML + "\n    </body>\n</html>"
     return code;
+}
+function get_source_code(code) {
+    //TODO: hacer una función que obtenga el codigo
+    //y lo divida en dos partes, el head y el body,
+    //Lo demás puede ser omitido. Tras obtener esos
+    //dos strings sustituir por su correspondiente
+    //innerHTML ("document.head.innerHTML", 
+    //"document.body.innerHTML").
 }
